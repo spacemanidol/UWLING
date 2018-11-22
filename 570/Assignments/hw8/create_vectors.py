5f37b655cb2f7846cd156a389a7028b50ac6067c
Your Name <you@example.com>
2018-11-21 16:42:18 -0800

    hw8 570

--- 570/Assignments/hw8/create_vectors.py
+++ 570/Assignments/hw8/create_vectors.py
@@ -1,7 +1,7 @@
 import sys
 import re
 import os
-def proc(input_filename, target_label):
+def proc(input_filename):
     header = 0
     features = {}
     with open(input_filename, 'r') as f:
@@ -20,37 +20,43 @@ def proc(input_filename, target_label):
                 header = 1            
     features = sorted(features.items(), key = lambda x:x[0])
     return features
-def train_test_split(filenames, directory, ratio):
+def train_test_split(directory, ratio):
+    filenames = os.listdir(directory)
     real_files =  []
-    print(len(filenames))
     for filename in filenames:
         if os.path.isfile(os.path.join(directory,filename)):
-            real_files.append(os.path.join(directory,filename))
+            real_files.append(filename)
     train_size = int(len(real_files) * float(ratio))
-    print(len(real_files[:train_size]))
-    print(len(real_files[train_size:]))
     return real_files[:train_size], real_files[train_size:]
+def write_result(input_filename, target_label, output_filename, features):
+    out_f = open(output_filename, 'a')
+    out_f.write(input_filename + ' ' + target_label)
+    for item in features:
+        out_f.write(' ' + item[0] + ' ' + str(item[1]))
+    out_f.write("\n")
+    out_f.close()
 def main():
+    dirs = []
+    for i in range(4, len(sys.argv)):
+        dirs.append(sys.argv[i])
     ratio = sys.argv[3]
-    for directory in sys.argv[4:]:
-        train_filenames, test_filenames = train_test_split(os.listdir(directory), directory, ratio)
+    for directory in dirs:
+        train_filenames, test_filenames = train_test_split(directory, ratio)
         with open(sys.argv[1], 'a') as w:
             for filename in train_filenames:
-                features = proc(filename, directory)
-                w.write("{}".format(filename))
-                for word in features:
-                    w.write(" {} {}".format(word[0], word[1]))
+                file_path = os.path.join(directory, filename) 
+                features = proc(file_path)
+                w.write("{} {}".format(filename, os.path.basename(directory)))
+                for item in features:
+                    w.write(" {} {}".format(item[0], item[1]))
                 w.write('\n')
         with open(sys.argv[2], 'a') as w:
             for filename in test_filenames:
-                features = proc(filename, directory)
-                w.write("{}".format(filename))
-                for word in features:
-                    w.write(" {} {}".format(word[0], word[1]))
+                file_path = os.path.join(directory, filename) 
+                features = proc(file_path)
+                w.write("{} {}".format(filename, os.path.basename(directory)))
+                for item in features:
+                    w.write(" {} {}".format(item[0], item[1]))
                 w.write('\n')
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: create_vectors.py <input_filename> <target_label> <output_filename>")
-        exit(-1)
-    else:
-        main()
+    main()
