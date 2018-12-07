8b7c2097f07621193fc5166119a4b2ebbb802f4b
Your Name <you@example.com>
2018-12-06 18:28:57 -0800

    hw10

---
+++ 570/Assignments/hw10/hw10.cmd
@@ -0,0 +1,9 @@
+universe = vanilla
+executable = /bin/bash
+getenv = true
+output = myoutput.out
+error = myerror.err
+log = log
+arguments = "maxext_tagger.sh wsj_sec0.word_pos test.word_pos 1 1 1_1"
+transfer_executable=false
+queue
