85207dbb59bc255a06c6773c6a4952525130a487
Your Name <you@example.com>
2019-01-22 16:14:28 -0800

    fixed run.cmd

--- 572/Assignments/hw2/run.cmd
+++ 572/Assignments/hw2/run.cmd
@@ -2,6 +2,7 @@ universe = vanilla
 executable = /bin/bash
 getenv = true
 log = log
+error = myerror
 arguments = "./build_dt.sh train.vectors.txt test.vectors.txt 1 0 model_file sys_output > acc_file"
 transfer_executable=false
 queue
