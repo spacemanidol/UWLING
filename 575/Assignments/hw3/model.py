16ebea8cb5ec9e98c9d6e00d2ecd328952185955
Daniel Campos <dacamp@microsoft.com>
2019-04-16 00:21:42 -0700

    Initial Experiments with models for HW3. Naive CNN

---
+++ 575/Assignments/hw3/model.py
@@ -0,0 +1,156 @@
+from __future__ import print_function
+
+import os
+import sys
+import numpy as np
+
+from keras.preprocessing.text import Tokenizer, one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.initializers import Constant
+from keras.preprocessing import sequence
+from keras.utils import to_categorical
+from keras.models import Model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation , Input, GlobalMaxPooling1D, MaxPooling1D, Embedding, Conv1D
+
+# set parameters:
+BATCH_SIZE = 128
+EPOCHS = 5
+EMBEDDING_DIM = 200
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NUM_WORDS = 20000
+VALIDATION_SPLIT = 0.1
+VECTOR_FILENAME = '../vectors.txt'
+TRAIN_FILENAME = 'atis.train.w-intent.IOB'
+TEST_FILENAME = 'atis.test.w-intent.IOB'
+
+
+#Run Code
+print('Indexing word vectors.')
+embeddings_index = {}
+with open(VECTOR_FILENAME, 'r') as f:
+    for l in f:
+        l = l.strip().split(' ')
+        embeddings_index[l[0]] = np.array([float(i) for i in l[1:]])
+print('Found %s word vectors.' % len(embeddings_index))
+
+print('Processing train dataset')
+texts, labels = [], []
+with open(TRAIN_FILENAME, encoding='latin-1') as f:
+    for l in f:
+        l = l.strip().split('\t')
+        texts.append(l[0])
+        labels.append(l[1].split(' ')[-1].split('#')[0]) # for any class that has multiple lables we take just the first
+
+label2idx, int_labels = {}, []
+idx = 0
+allLabels = set(labels)
+for label in allLabels:
+    label2idx[label] = idx
+    idx += 1
+for label in labels:
+    int_labels.append(label2idx[label])
+labels = int_labels
+
+print('Found %s training examples.' % len(texts))
+tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+word_index = tokenizer.word_index
+data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+labels = to_categorical(np.asarray(labels))
+print('Found %s unique tokens.' % len(word_index))
+print('Shape of data tensor:', data.shape)
+
+# split the data into a training set and a validation set
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+num_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+x_train = data[:-num_validation_samples]
+y_train = labels[:-num_validation_samples]
+x_val = data[-num_validation_samples:]
+y_val = labels[-num_validation_samples:]
+
+print('Preparing embedding matrix and One-Hot Matrix')
+num_words = min(MAX_NUM_WORDS, len(word_index)) + 1
+embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+one_hot_matrix =  np.zeros((num_words, num_words))
+for word, i in word_index.items():
+    if i > MAX_NUM_WORDS:
+        continue
+    embedding_vector = embeddings_index.get(word)
+    one_hot_matrix[i][one_hot(word, num_words)] = 1
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
+
+print('Processing test dataset')
+texts, labels = [], []
+with open(TEST_FILENAME, encoding='latin-1') as f:
+    for l in f:
+        l = l.strip().split('\t')
+        texts.append(l[0])
+        labels.append(l[1].split(' ')[-1].split('#')[0]) # for any class that has multiple lables we take just the first
+
+int_labels = []
+for label in labels:
+    if label not in label2idx:
+        int_labels.append(-1)
+    else:
+        int_labels.append(label2idx[label])
+labels = int_labels
+
+print('Found %s test examples.' % len(texts))
+tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+x_test = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+y_test = to_categorical(np.asarray(labels))
+
+
+print('Running with Glove Embeddings')
+print('Training model.')
+embedding_layer = Embedding(num_words, EMBEDDING_DIM, embeddings_initializer=Constant(embedding_matrix), input_length=MAX_SEQUENCE_LENGTH, trainable=False)
+sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedded_sequences = embedding_layer(sequence_input)
+x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = GlobalMaxPooling1D()(x)
+x = Dense(128, activation='relu')(x)
+preds = Dense(len(allLabels), activation='softmax')(x)
+model = Model(sequence_input, preds)
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['acc'])
+model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(x_val, y_val))
+
+print('Evaluating model')
+score = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=1)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+print('Running with OneHot Vector')
+print('Training model.')
+embedding_layer = Embedding(num_words, num_words, embeddings_initializer=Constant(one_hot_matrix), input_length=MAX_SEQUENCE_LENGTH, trainable=False)
+sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedded_sequences = embedding_layer(sequence_input)
+x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = GlobalMaxPooling1D()(x)
+x = Dense(128, activation='relu')(x)
+preds = Dense(len(allLabels), activation='softmax')(x)
+model = Model(sequence_input, preds)
+model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['acc'])
+model.fit(x_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(x_val, y_val))
+
+
+print('Evaluating model.')
+score = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=1)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
